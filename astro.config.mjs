01372789d523f95ef263321994c29aa399eb5db3
Samuel Boguslawski <sam.bogu@gmail.com>
2023-03-28 12:02:48 +0200

    🚸 bring back heading links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+import { rehypeHeadingIds } from '@astrojs/markdown-remark'
 
-import sitemap from "@astrojs/sitemap";
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://codecookies.xyz',
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  markdown: {
+    rehypePlugins: [rehypeHeadingIds, [rehypeAutolinkHeadings, { behavior: 'wrap' }]],
+  }
 });
