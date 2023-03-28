1c7b04db9851545f8912634171fb19458600b063
Samuel Boguslawski <sam.bogu@gmail.com>
2023-03-28 12:32:23 +0200

    🚧  Add possibility for adding table of contents

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import { rehypeHeadingIds } from '@astrojs/markdown-remark'
 
+import remarkToc from 'remark-toc'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 
 // https://astro.build/config
@@ -9,6 +10,7 @@ export default defineConfig({
   site: 'https://codecookies.xyz',
   integrations: [sitemap()],
   markdown: {
+    remarkPlugins: [[remarkToc, { heading: 'Contents' }]],
     rehypePlugins: [rehypeHeadingIds, [rehypeAutolinkHeadings, { behavior: 'wrap' }]],
   }
 });
